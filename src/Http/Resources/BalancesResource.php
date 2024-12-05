357a6a5feedf82baeb8a5cc140da9c745cea09dd
Vinoth Kannan <svkn.87@gmail.com>
2024-12-05 14:30:54 +0530

    FEATURE: add api request for balances history

--- src/Http/Resources/BalancesResource.php
+++ src/Http/Resources/BalancesResource.php
@@ -5,11 +5,17 @@ namespace Vinkas\Airwallex\Http\Resources;
 use Saloon\Http\BaseResource;
 use Saloon\Http\Response;
 use Vinkas\Airwallex\Http\Balances\CurrentRequest;
+use Vinkas\Airwallex\Http\Balances\HistoryRequest;
 
 class BalancesResource extends BaseResource
 {
     public function getCurrent(): Response
     {
         return $this->connector->send(new CurrentRequest());
+    }
+
+    public function getHistory(): Response
+    {
+        return $this->connector->send(new HistoryRequest());
     }
 }
