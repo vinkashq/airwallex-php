69bebc3b421300191c02cee4cab297a6bd7591b1
Vinoth Kannan <svkn.87@gmail.com>
2024-12-05 17:53:51 +0530

    FEATURE: add optional parameters to the balances history endpoint

--- src/Http/Balances/HistoryRequest.php
+++ src/Http/Balances/HistoryRequest.php
@@ -9,6 +9,31 @@ class HistoryRequest extends Request
 {
     protected Method $method = Method::GET;
 
+    public function __construct(readonly ?string $from = null, readonly ?string $to = null, readonly ?string $page = null, readonly ?string $currency = null) { }
+
+    protected function defaultQuery(): array
+    {
+        $query = parent::defaultQuery();
+
+        if (isset($this->from)) {
+            $query['from'] = $this->from;
+        }
+
+        if (isset($this->to)) {
+            $query['to'] = $this->to;
+        }
+
+        if (isset($this->page)) {
+            $query['page'] = $this->page;
+        }
+
+        if (isset($this->currency)) {
+            $query['currency'] = $this->currency;
+        }
+
+        return $query;
+    }
+
     public function resolveEndpoint(): string
     {
         return 'balances/history';
--- src/Http/Resources/BalancesResource.php
+++ src/Http/Resources/BalancesResource.php
@@ -14,8 +14,8 @@ class BalancesResource extends BaseResource
         return $this->connector->send(new CurrentRequest());
     }
 
-    public function getHistory(): Response
+    public function getHistory(?string $from = null, ?string $to = null, ?string $page = null, ?string $currency = null): Response
     {
-        return $this->connector->send(new HistoryRequest());
+        return $this->connector->send(new HistoryRequest($from, $to, $page, $currency));
     }
 }
